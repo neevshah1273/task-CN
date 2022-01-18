270b4a0cea057774c3c20607295372b976a1c8a9
neevshah1273 <neevshah1273@gmail.com>
2022-01-19 00:26:03 +0530

    Final Commit

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,25 +1,37 @@
 import { Card,ListGroup,ListGroupItem } from "react-bootstrap";
 
 const Cards = (props) => {
+
+    var date = new Date(props.eve.start_time * 1000);
+
+    let ss = date.toLocaleDateString();
     return(
         <div>
-            <Card style={{ width: '18rem' }}>
+            <Card style={{ width: '24rem' }}>
                 <Card.Img variant="top" src={props.eve.cover_picture} />
                 <Card.Body>
-                    <Card.Title>{props.eve.name}</Card.Title>
-                    <Card.Text>
+                    <Card.Title >{props.eve.name}</Card.Title>
+                    <Card.Text >
                         {props.eve.short_desc}
                     </Card.Text>
                 </Card.Body>
                 <ListGroup className="list-group-flush">
-                    <ListGroupItem>starts on</ListGroupItem>
+                    
+                    <ListGroupItem>starts on {ss}</ListGroupItem>
                     <ListGroupItem>Entry Fee {props.eve.fees} {props.eve.currency}</ListGroupItem>
                     <ListGroupItem>Venue {props.eve.venue}</ListGroupItem>
                 </ListGroup>
-                <Card.Body>
+                {props.eve.registration_status==="REGISTRATIONS_OPEN"?
+                    <Card.Body>
                     <Card.Link href="#">Register Now</Card.Link>
 
-                </Card.Body>
+                    </Card.Body>
+                    : 
+                    <div>
+
+                    </div>
+                }
+                
                 </Card>
         </div>
     )
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,18 +1,21 @@
 import axios from 'axios';
 import React,{ useState,useEffect } from "react";
 import Cards from "../Cards/Cards.js";
-import Navbar1 from "../Navbar/NavBar1";
-import Navbar2 from "../Navbar/NavBar2";
+import {Navbar,Nav,Container} from 'react-bootstrap';
+import Grid from '@material-ui/core/Grid';
 
 const Home = () => {
 
     const API = axios.create({baseURL:'https://api.codingninjas.com/api/v3'});
     const [tag, settag] = useState([]);
     
+    const [tp, settp] = useState(0);
 
     const catogory = [ 'ALL_EVENTS','CODING_EVENT', 'WEBINAR', 'BOOTCAMP_EVENT', 'WORKSHOP'];
 
     const [selectedcatogory, setselectedcatogory] = useState('ALL_EVENTS');
+
+    const [cp,setcp] = useState(1);
 
     const subcatogory = ['Upcoming', 'Archived' ,'All Time Favorites'];
 
@@ -48,7 +51,7 @@ const Home = () => {
             event_sub_category : selectedsubcatogory,
             tag_list : selectedtag.toString(),
             offset : offset
-        }}).then((res)=>setevents(res.data.data.events)).catch((err)=>console.log(err.message)).finally(()=>console.log(events))
+        }}).then((res)=>{setevents(res.data.data.events);settp(res.data.data.page_count)}).catch((err)=>console.log(err.message)).finally(()=>{console.log(events)})
 
 
 
@@ -67,9 +70,51 @@ const Home = () => {
                 return stag!==e.target.value;
             }))
         }
-        getevents();
-        console.log(selectedtag);
+        //getevents();
+        //console.log(selectedtag);
     }
+
+    useEffect(()=>{
+        getevents();
+    },[selectedtag]);
+
+
+    const handlecat = (e) => {
+        setselectedcatogory(e.target.value);
+        //getevents();
+    }
+
+    useEffect(()=>{
+        getevents();
+    },[selectedcatogory]);
+
+    const handlesubcat = (e) => {
+        setselectedsubcatogory(e.target.value);
+        //getevents();
+    }
+
+    useEffect(()=>{
+        getevents();
+    },[selectedsubcatogory]);
+
+    const handleoffset = () =>{
+        if(cp<tp){
+            setoffset(cp*20);
+        }
+    }
+
+    useEffect(()=>{
+        getevents();
+    },[offset]);
+
+    
+    var bgColors = { "Default": "#81b71a",
+                    "Blue": "#00B1E1",
+                    "Cyan": "#37BC9B",
+                    "Green": "#8CC152",
+                    "Red": "#E9573F",
+                    "Yellow": "#F6BB42",
+};
 
 
 
@@ -77,18 +122,52 @@ const Home = () => {
         <div>
             <div>
                 
-                <Navbar1/>
-                <Navbar2/>
+                <Navbar bg="dark" variant="dark" className='text-white'>
+                    {catogory.map((cat)=>
+                        <div className='px-3'>
+                        <button className="rounded px-2" style={{backgroundColor: bgColors.Yellow}}  value={cat} onClick={handlecat}>{cat}</button>
 
-                <div className="tags">
-                    {tag.map((it)=>
-                        <button value={it} onClick={handlechange} style={{backgroundColor:'white'}}>{it}</button>
+                        </div>
                     )}
+                </Navbar>
+                <Navbar bg="primary" variant="dark" className='text-white'>
+                    
+                    {subcatogory.map((cat)=>
+                        <div className='px-5'>
+                        <button className="rounded px-2" value={cat} onClick={handlesubcat}>{cat}</button>
+
+                        </div>
+                    )}
+                </Navbar>
+                
+                <div className="container pt-2 pb-2">
+                    <div className="row">
+                        <div className="col-9">
+                            <Grid container>
+                            {events.map((eve)=>
+                                <Grid className="pt-3" item xs={6}><Cards eve={eve}/></Grid>
+                                
+                            )}
+                            </Grid>
+
+                        </div>
+                        <div className="col-3">
+                            
+                            <div className="tags row">
+                                {tag.map((it)=>
+                                    <button className="rounded-pill pb-1 pt-1" value={it} onClick={handlechange} style={{backgroundColor:'white'}}>{it}</button>
+                                )}
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
-                {events.map((eve)=>
-                    <Cards eve={eve}/>
-                )}
+                <div className='d-flex justify-content-center'>
+                    <input type="number" value={cp} max={tp} onChange={e => setcp(e.target.value)}></input>
+                    of {tp}
+                    <button onClick={handleoffset}> Go</button>
+                </div>
+
                 
                 
             </div>
